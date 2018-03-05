90ff9acfd452849abc32c0d905d6a6a5cf83c0ab
Matthew-Yinuo <matthewhoth@gmail.com>
2018-03-04 19:31:41 -0700

    First commit

---
+++ slack-clone-client/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+
+import { ApolloProvider } from 'react-apollo';
+import registerServiceWorker from './registerServiceWorker';
+import Routes from './routes';
+
+
+const client = new ApolloClient({
+  link: createHttpLink({ uri: 'http://localhost:8081/graphql' }),
+  cache: new InMemoryCache(),
+});
+
+const App = (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
+registerServiceWorker();
