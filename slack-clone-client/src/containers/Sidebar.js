cf094c077cbb12dbef118bce2be5156d1d557a89
Matthew Hoth <matthewhoth@gmail.com>
2018-06-11 16:19:07 +0800

    Create a button that pops up a modal. Click outside to close

--- slack-clone-client/src/containers/Sidebar.js
+++ slack-clone-client/src/containers/Sidebar.js
@@ -3,42 +3,62 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import findIndex from 'lodash/findIndex';
 import decode from 'jwt-decode';
-
 import Channels from '../components/Channels';
 import Teams from '../components/Teams';
+import AddChannelModal from '../components/addChannelModal';
 
-const Sidebar = ({ data: { loading, allTeams }, currentTeamId }) => {
-  if (loading) {
-    return null;
+class Sidebar extends React.Component {
+
+  state={
+    openAddChannelModal: false,
   }
 
-  const teamIdx = currentTeamId ? findIndex(allTeams, ['id', parseInt(currentTeamId, 10)]) : 0;
-  const team = allTeams[teamIdx];
-  let username = '';
-  try {
-    const token = localStorage.getItem('token');
-    const { user } = decode(token);
-    // eslint-disable-next-line prefer-destructuring
-    username = user.username;
-  } catch (err) {}
+  handleCloseChannelClick= () => {
+    this.setState({ openAddChannelModal: false });
+  }
 
-  return [
-    <Teams
-      key="team-sidebar"
-      teams={allTeams.map(t => ({
-        id: t.id,
-        letter: t.name.charAt(0).toUpperCase(),
-      }))}
-    />,
-    <Channels
-      key="channels-sidebar"
-      teamName={team.name}
-      username={username}
-      channels={team.channels}
-      users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
-    />,
-  ];
-};
+  handleAddChannelClick= () => {
+    this.setState({ openAddChannelModal: true });
+  }
+
+
+  render(){
+
+    const { data: { loading, allTeams }, currentTeamId } = this.props;
+    if (loading) {
+      return null;
+    }
+
+    const teamIdx = currentTeamId ? findIndex(allTeams, ['id', parseInt(currentTeamId, 10)]) : 0;
+    const team = allTeams[teamIdx];
+    let username = '';
+    try {
+      const token = localStorage.getItem('token');
+      const { user } = decode(token);
+      // eslint-disable-next-line prefer-destructuring
+      username = user.username;
+    } catch (err) {}
+
+    return [
+      <Teams
+        key="team-sidebar"
+        teams={allTeams.map(t => ({
+          id: t.id,
+          letter: t.name.charAt(0).toUpperCase(),
+        }))}
+      />,
+      <Channels
+        key="channels-sidebar"
+        teamName={team.name}
+        username={username}
+        channels={team.channels}
+        users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
+        onAddChannelClick={this.handleAddChannelClick}
+        />,
+      <AddChannelModal open={this.state.openAddChannelModal} onClose={this.handleCloseChannelClick} key='sidebar-add-channel-modal'></AddChannelModal>
+    ];
+  };
+}
 
 const allTeamsQuery = gql`
   {
