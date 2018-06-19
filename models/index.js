318a5a23071601bb9986c9a2ebee633e9d993f1e
Matthew Hoth <matthewhoth@gmail.com>
2018-06-19 19:07:42 +0800

    Get the values store in state and start compiling functions for sending messages

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('SlackDB', 'postgres', 'slack', {
+const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
   dialect: 'postgres',
   operatorsAliases: Sequelize.Op,
   define: {
--- slack-clone-client/src/components/SendMessage.js
+++ slack-clone-client/src/components/SendMessage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Input } from 'semantic-ui-react';
+import { withFormik } from "formik";
 
 const SendMessageWrapper = styled.div`
   grid-column: 3;
@@ -8,8 +9,49 @@ const SendMessageWrapper = styled.div`
   margin: 20px;
 `;
 
-export default ({ channelName }) => (
+const ENTER_KEY = 13;
+
+
+const SendMessage = ({
+  onKeyDown={(e) => {
+  if (e.keyCode === ENTER_KEY && !isSubmitting) {
+    handleSubmit(e);
+      }
+    }
+  }
+  channelName,
+  values,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting,
+  resetForm,
+  }) => (
   <SendMessageWrapper>
-    <Input fluid placeholder={`Message #${channelName}`} />
+    <Input name="message" value={values.message} onBlur={handleBlur} onChange={handleChange} fluid placeholder={`Message #${channelName}`} />
   </SendMessageWrapper>
 );
+
+const createMessageMutation = gql`
+  mutation($channelId: Int!, $text: String!) {
+    createMessage(channelId: $channelId, text: $text)
+  }
+`;
+
+export default compose(
+  graphql(createMessageMutation),
+  withFormik({
+    mapPropsToValues: () => ({ message: '' }),
+    handleSubmit: async (values, { props: { channelId, mutate }, setSubmitting, resetForm }) => {
+      if (!values.message || !values.message.trim()) {
+        setSubmitting(false);
+        return;
+      }
+
+      await mutate({
+        variables: { channelId, text: values.message },
+      });
+      resetForm(false);
+    },
+  }),
+)(SendMessage);
