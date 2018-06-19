ec15dc1ca9d52c94e40039292c2627160db86492
Matthew Hoth <matthewhoth@gmail.com>
2018-06-19 21:30:23 +0800

    Merge branch 'Send_message_form'
    Messages showing up and queries are being called correctly.

--- resolvers/message.js
+++ resolvers/message.js
@@ -1,22 +1,33 @@
+import requiresAuth from "../permissions";
+
 export default {
-
-  Query : {
-    messages: async(parent, args, { models, user }) => [],
+  Message: {
+    user: ({ userId }, args, { models }) =>
+      models.User.findOne({ where: { id: userId } }, { raw: true })
   },
-
-
+  Query: {
+    messages: requiresAuth.createResolver(
+      async (parent, { channelId }, { models }) =>
+        models.Message.findAll(
+          { order: [["created_at", "ASC"]], where: { channelId } },
+          { raw: true }
+        )
+    )
+  },
   Mutation: {
-    createMessage: async (parent, args, { models, user }) => {
-      try {
-        await models.Message.create({
-          ...args,
-          userId: user.id,
-        });
-        return true;
-      } catch (err) {
-        console.log(err);
-        return false;
+    createMessage: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Message.create({
+            ...args,
+            userId: user.id
+          });
+          return true;
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
       }
-    },
-  },
+    )
+  }
 };
