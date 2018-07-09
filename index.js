8d096fbe85dee81c7db65f112a006a12ba6b543f
Matthew Hoth <matthewhoth@gmail.com>
2018-07-09 20:57:24 +0800

    Update index to set up subscriptions, installed dependencies

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
-import path from 'path';
-import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
-import cors from 'cors';
-import jwt from 'jsonwebtoken';
-import models from './models';
-import { refreshTokens } from './auth';
+import express from "express";
+import bodyParser from "body-parser";
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import { makeExecutableSchema } from "graphql-tools";
+import path from "path";
+import { fileLoader, mergeTypes, mergeResolvers } from "merge-graphql-schemas";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import { createServer } from "http";
+import { execute, subscribe } from "graphql";
+import { PubSub } from "graphql-subscriptions";
+import { SubscriptionServer } from "subscriptions-transport-ws";
 
-const SECRET = 'asasdf3jnl1kejd';
-const SECRET2 = 'asiasdfl1kejasdjlkfasdd';
+import models from "./models";
+import { refreshTokens } from "./auth";
 
-const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+const SECRET = "asiodfhoi1hoi23jnl1kejd";
+const SECRET2 = "asiodfhoi1hoi23jnl1kejasdjlkfasdd";
 
-const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, "./schema")));
+
+const resolvers = mergeResolvers(
+  fileLoader(path.join(__dirname, "./resolvers"))
+);
 
 const schema = makeExecutableSchema({
   typeDefs,
-  resolvers,
+  resolvers
 });
 
 const app = express();
 
-app.use(cors('*'));
+app.use(cors("*"));
 
 const addUser = async (req, res, next) => {
-  const token = req.headers['x-token'];
+  const token = req.headers["x-token"];
   if (token) {
     try {
       const { user } = jwt.verify(token, SECRET);
       req.user = user;
     } catch (err) {
-      const refreshToken = req.headers['x-refresh-token'];
-      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+      const refreshToken = req.headers["x-refresh-token"];
+      const newTokens = await refreshTokens(
+        token,
+        refreshToken,
+        models,
+        SECRET,
+        SECRET2
+      );
       if (newTokens.token && newTokens.refreshToken) {
-        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
-        res.set('x-token', newTokens.token);
-        res.set('x-refresh-token', newTokens.refreshToken);
+        res.set("Access-Control-Expose-Headers", "x-token, x-refresh-token");
+        res.set("x-token", newTokens.token);
+        res.set("x-refresh-token", newTokens.refreshToken);
       }
       req.user = newTokens.user;
     }
@@ -47,7 +60,7 @@ const addUser = async (req, res, next) => {
 
 app.use(addUser);
 
-const graphqlEndpoint = '/graphql';
+const graphqlEndpoint = "/graphql";
 
 app.use(
   graphqlEndpoint,
@@ -58,13 +71,28 @@ app.use(
       models,
       user: req.user,
       SECRET,
-      SECRET2,
-    },
-  })),
+      SECRET2
+    }
+  }))
 );
 
-app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
-models.sequelize.sync().then(() => {
-  app.listen(8081);
+const server = createServer(app);
+
+models.sequelize.sync({}).then(() => {
+  server.listen(8081, () => {
+    // eslint-disable-next-line no-new
+    new SubscriptionServer(
+      {
+        execute,
+        subscribe,
+        schema
+      },
+      {
+        server,
+        path: "/subscriptions"
+      }
+    );
+  });
 });
