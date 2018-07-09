7348b81e099a3b4ff18f755f0dbdcb8ce47c5d63
Matthew Hoth <matthewhoth@gmail.com>
2018-07-09 23:19:36 +0800

    typo

--- slack-clone-client/src/index.js
+++ slack-clone-client/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import 'semantic-ui-css/semantic.min.css';
-import Routes from './routes';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import "semantic-ui-css/semantic.min.css";
 
+import Routes from "./routes";
+import registerServiceWorker from "./registerServiceWorker";
+import client from "./apollo";
 
 const App = (
   <ApolloProvider client={client}>
@@ -12,5 +13,5 @@ const App = (
   </ApolloProvider>
 );
 
-ReactDOM.render(App, document.getElementById('root'));
+ReactDOM.render(App, document.getElementById("root"));
 registerServiceWorker();
