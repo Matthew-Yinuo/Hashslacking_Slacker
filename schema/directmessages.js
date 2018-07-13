e66a7f851132dc01bf380767dd7e11d2e7c58235
Matthew Hoth <matthewhoth@gmail.com>
2018-07-13 19:17:48 +0800

    Create the model and backend schema for direct messaging.

---
+++ schema/directmessages.js
@@ -0,0 +1,18 @@
+export default `
+  type DirectMessage {
+    id: Int!
+    text: String!
+    sender: User!
+    receiverId: Int!
+    created_at: String!
+  }
+
+  type Query {
+    directmessages: [DirectMessage!]!
+  }
+
+  type Mutation {
+    createDirectMessage(receiverId: Int!,text: String!): Boolean!
+  }
+
+`;
