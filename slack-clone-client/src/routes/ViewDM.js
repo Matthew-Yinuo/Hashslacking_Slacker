86c3a055218008cdd9c6c105b1463ca2c4f2b92c
Matthew Hoth <matthewhoth@gmail.com>
2018-07-13 23:52:06 +0800

    Quarantine a display bug.

--- slack-clone-client/src/routes/ViewDM.js
+++ slack-clone-client/src/routes/ViewDM.js
@@ -4,49 +4,61 @@ import findIndex from 'lodash/findIndex';
 import { Redirect } from 'react-router-dom';
 import SendMessage from '../components/SendMessage';
 import AppLayout from '../components/AppLayout';
+import Header from "../components/Header";
 import Sidebar from '../containers/Sidebar';
+import DMContainer from '../containers/DMContainer';
 import { meQuery } from '../graphql/team';
 import gql from 'graphql-tag';
 
-const ViewTeam = ({ data: { loading, me }, match: { params: { teamId, userId } } }) => {
-    if (loading) {
-        return null;
-    }
+const ViewTeam = ({ mutate, data: { loading, me }, match: { params: { teamId, userId } } }) => {
+  if (loading) {
+    return null;
+  }
 
-    const { username, teams } = me;
+  const { username, teams } = me;
 
-    if (!teams.length) {
-        return <Redirect to="/create-team" />;
-    }
+  if (!teams.length) {
+    return <Redirect to="/create-team" />;
+  }
 
-    const teamIdInteger = parseInt(teamId, 10);
-    const teamIdx = teamIdInteger ? findIndex(teams, ['id', teamIdInteger]) : 0;
-    const team = teamIdx === -1 ? teams[0] : teams[teamIdx];
+  const teamIdInteger = parseInt(teamId, 10);
+  const teamIdx = teamIdInteger ? findIndex(teams, ['id', teamIdInteger]) : 0;
+  const team = teamIdx === -1 ? teams[0] : teams[teamIdx];
 
-    return (
-        <AppLayout>
-            <Sidebar
-                teams={teams.map(t => ({
+  return (
+    <AppLayout>
+        <Sidebar
+            teams={teams.map(t => ({
                     id: t.id,
                     letter: t.name.charAt(0).toUpperCase(),
                 }))}
-                team={team}
-                username={username}
+            team={team}
+            username={username}
             />
-            {/* <Header channelName={channel.name} />
-      <MessageContainer channelId={channel.id} /> */}
-            <SendMessage onSubmit={() => { }} placeholder={userId} />
-        </AppLayout>
-    );
+        <Header channelName={"Name Here"} />
+        <DMContainer teamId={teamId}  userId={userId}/>
+        <SendMessage
+            onSubmit={async (text) => {
+            await mutate({
+            variables: {
+                text,
+                receiverId: userId,
+            },
+            });
+            }}
+            placeholder={userId}
+            />
+      </AppLayout>
+  );
 };
 
-const createMessageMutation = gql`
-  mutation($channelId: Int!, $text: String!) {
-    createMessage(channelId: $channelId, text: $text)
+const createDirectMessageMutation = gql`
+    mutation($receiverId: Int!, $text: String!) {
+    createDirectMessage(receiverId: $receiverId, text: $text)
   }
 `;
 
 export default compose(
-    graphql(meQuery, { options: { fetchPolicy: 'network-only' } }),
-    graphql(createMessageMutation),
+  graphql(meQuery, { options: { fetchPolicy: 'network-only' } }),
+  graphql(createDirectMessageMutation),
 )(ViewTeam);
