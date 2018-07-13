3e56f54ee145e1a989776d61b67ffa443000163e
Matthew Hoth <matthewhoth@gmail.com>
2018-07-13 20:54:40 +0800

    Fix permissions to prevent users from subscribing to threads they are not members of

--- slack-clone-client/src/routes/ViewDM.js
+++ slack-clone-client/src/routes/ViewDM.js
@@ -1,33 +1,26 @@
-import React from "react";
-import { compose, graphql } from "react-apollo";
-import findIndex from "lodash/findIndex";
-import { Redirect } from "react-router-dom";
-import Header from "../components/Header";
-import SendMessage from "../components/SendMessage";
-import AppLayout from "../components/AppLayout";
-import Sidebar from "../containers/Sidebar";
-import MessageContainer from "../containers/MessageContainer";
-import { meQuery } from "../graphql/team";
+import React from 'react';
+import { compose, graphql } from 'react-apollo';
+import findIndex from 'lodash/findIndex';
+import { Redirect } from 'react-router-dom';
+import SendMessage from '../components/SendMessage';
+import AppLayout from '../components/AppLayout';
+import Sidebar from '../containers/Sidebar';
+import { meQuery } from '../graphql/team';
 import gql from 'graphql-tag';
 
-const ViewTeam = ({
-    data: { loading, me, ...otherProps },
-    match: {
-        params: { teamId, userId }
-    }
-}) => {
+const ViewTeam = ({ data: { loading, me }, match: { params: { teamId, userId } } }) => {
     if (loading) {
         return null;
     }
 
-    const { teams, username } = me;
+    const { username, teams } = me;
 
     if (!teams.length) {
         return <Redirect to="/create-team" />;
     }
 
     const teamIdInteger = parseInt(teamId, 10);
-    const teamIdx = teamIdInteger ? findIndex(teams, ["id", teamIdInteger]) : 0;
+    const teamIdx = teamIdInteger ? findIndex(teams, ['id', teamIdInteger]) : 0;
     const team = teamIdx === -1 ? teams[0] : teams[teamIdx];
 
     return (
@@ -35,23 +28,25 @@ const ViewTeam = ({
             <Sidebar
                 teams={teams.map(t => ({
                     id: t.id,
-                    letter: t.name.charAt(0).toUpperCase()
+                    letter: t.name.charAt(0).toUpperCase(),
                 }))}
                 team={team}
                 username={username}
             />
-            <SendMessage onSubmit={() => {}} placeholder ={userId}/>
+            {/* <Header channelName={channel.name} />
+      <MessageContainer channelId={channel.id} /> */}
+            <SendMessage onSubmit={() => { }} placeholder={userId} />
         </AppLayout>
     );
 };
 
-const createDirectMessageMutation = gql`
+const createMessageMutation = gql`
   mutation($channelId: Int!, $text: String!) {
     createMessage(channelId: $channelId, text: $text)
   }
 `;
 
 export default compose(
-    graphql(meQuery, { options: { fetchPolicy: "network-only" } }),
-    graphql(createDirectMessageMutation),
+    graphql(meQuery, { options: { fetchPolicy: 'network-only' } }),
+    graphql(createMessageMutation),
 )(ViewTeam);
